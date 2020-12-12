b472936a1d76a022885e6b529b33498ba6788838
haritharani-mv <haritharani.mv@gmail.com>
2020-12-12 10:00:55 +0530

    assignment

---
+++ asserts/assignment.js
@@ -0,0 +1,25 @@
+function create(){
+    alert('good morning');
+}
+
+function find(name){
+    alert('hey' + name);
+}
+
+// calling function 
+create();
+
+// calling function
+find('haritha');
+
+
+text3Element.addEventListener('click', create);
+
+function brand(a, b, c){
+    let d = a + b + c;
+    alert('result is:'+d);
+    console.log(d);
+}
+
+brand('hello', 'good', 'evening');
+
